af803f9e2b00ba7af6e33d0563e0e2953f531710
123123 <15651875728@163.com>
2019-06-02 00:08:09 +0800

    tav to blank

--- trial/va/add3.c
+++ trial/va/add3.c
@@ -7,43 +7,43 @@
 
 int has_next(const char* s, int* ppos, int* ptype)
 {
-	int pos = *ppos;
-	while (s[pos] == ' ') pos++;
-	
-	const char* cur_arg = s + pos;
-	*ppos = pos + 2;
-	
-	if (strncmp("%d", cur_arg, 2) == 0) {
-		return (*ptype) = 1;
-	} 
-	if (strncmp("%s", cur_arg, 2) == 0) {
-		return (*ptype) = 2;
-	}
-	
-	return 0;
+    int pos = *ppos;
+    while (s[pos] == ' ') pos++;
+    
+    const char* cur_arg = s + pos;
+    *ppos = pos + 2;
+    
+    if (strncmp("%d", cur_arg, 2) == 0) {
+        return (*ptype) = 1;
+    } 
+    if (strncmp("%s", cur_arg, 2) == 0) {
+        return (*ptype) = 2;
+    }
+    
+    return 0;
 }
 
 int add(const char *format, ...)
 {
-	va_list ap;
-	va_start(ap, format);
-	
-	int sum = 0, arg;
-	int pos = 0, type;
-	while (has_next(format, &pos, &type)) {
-		// get param
-		if (type == 1) {			// int
-			arg = va_arg(ap, int);
-		} else if (type == 2) {		// string
-			arg = atoi(va_arg(ap, char*));
-		}
-		// add param
-		sum += arg;
-	}
-	
-	va_end(ap);
-	
-	return sum;
+    va_list ap;
+    va_start(ap, format);
+    
+    int sum = 0, arg;
+    int pos = 0, type;
+    while (has_next(format, &pos, &type)) {
+        // get param
+        if (type == 1) {			// int
+            arg = va_arg(ap, int);
+        } else if (type == 2) {		// string
+            arg = atoi(va_arg(ap, char*));
+        }
+        // add param
+        sum += arg;
+    }
+    
+    va_end(ap);
+    
+    return sum;
 }
 
 int main()
@@ -52,5 +52,5 @@ int main()
     printf("str + str: sum = %d\n", add("%s %s", "11", "22"));
     printf("int + str: sum = %d\n", add("%d %s", 11, "22"));
 
-	return 0;
+    return 0;
 }
